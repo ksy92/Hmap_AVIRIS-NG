e2809e5c51d9103f0d2ac259f435aa8dfc0f1cd6
Siyoon Kwon <35686126+siyoonk@users.noreply.github.com>
2024-08-01 16:06:09 +0900

    Add files via upload

--- Code/WLD_DEPTH_MAPPING_FUNCTIONS.py
+++ Code/WLD_DEPTH_MAPPING_FUNCTIONS.py
@@ -22,16 +22,17 @@ from sklearn.base import clone, is_classifier
 
 
 def regression_results(y_true, y_pred):
-
     errors = abs(np.array(y_true).reshape(-1) - np.array(y_pred).reshape(-1))
-    explained_variance=metrics.explained_variance_score(y_true, y_pred)
-    mean_absolute_error=np.mean(errors**2)
-    mse=metrics.mean_squared_error(y_true, y_pred) 
-    r2=metrics.r2_score(y_true, y_pred)
+    explained_variance = metrics.explained_variance_score(y_true, y_pred)
+    mean_absolute_error = np.mean(errors)
+    mse = metrics.mean_squared_error(y_true, y_pred)
+    r2 = metrics.r2_score(y_true, y_pred)
     mape = 100 * (errors / np.array(y_true).reshape(-1))
     mape = np.mean(mape)
+    mean_bias_error = np.mean(np.array(y_pred).reshape(-1) - np.array(y_true).reshape(-1))
     
-    return round(explained_variance,4),  round(r2,4), round(mean_absolute_error,4),round(np.sqrt(mse),4),  round(mape,4)
+    return round(explained_variance, 4), round(r2, 4), round(mean_absolute_error, 4), round(np.sqrt(mse), 4), round(mape, 4), round(mean_bias_error, 4)
+
 
 def RFECV_RF(X,Y, step, cv, estimator, n_jobs):
     rfecv = RFECV(estimator=estimator, step=1, cv=5, scoring="neg_mean_squared_error", n_jobs=n_jobs)
@@ -47,34 +48,42 @@ def RFECV_RF(X,Y, step, cv, estimator, n_jobs):
     rfeindex = np.where(rfecv.support_ == True)[0]
     return dset, rfeindex
 
-def RFECV_PLSR(X,Y, step, cv, estimator):
+def RFECV_PLSR(X,Y, step, cv, n_comp):
 
     scoring="neg_mean_squared_error"
-    n_features_to_select = 91
-    estimator = PLSRegression(n_components=n_features_to_select)   
+    # comp_range = np.arange(8)+2
+    # n_features_to_select = 91
+    estimator = PLSRegression(n_components=n_comp)
     cv = check_cv(cv, Y, classifier=is_classifier(estimator))
     scores = []
     n_features_list = []
-    # Generate the different feature subsets and train/evaluate models
-    for n_features_to_select in range(1, X.shape[1] + 1, step):
-        rfe = RFE(estimator=PLSRegression(n_components=n_features_to_select), n_features_to_select=n_features_to_select, step=step)
-        rfe.fit(X, Y)
-        # Cross-validate the model and store the average score
-        cv_scores = cross_val_score(rfe.estimator_, X[:, rfe.support_], Y, cv=cv, scoring=scoring)
-        scores.append(np.mean(cv_scores))
-        n_features_list.append(n_features_to_select)
+    # comp_scores = []
+    # comp_featuers = []
+    # # Generate the different feature subsets and train/evaluate models
+    # for comp in comp_range:
+    for n_features_to_select in range(n_comp, X.shape[1] + 1, step):
+            rfe = RFE(estimator=estimator, n_features_to_select=n_features_to_select, step=step)
+            rfe.fit(X, Y)
+            # Cross-validate the model and store the average score
+            cv_scores = cross_val_score(rfe.estimator_, X[:, rfe.support_], Y, cv=cv, scoring=scoring)
+            scores.append(np.mean(cv_scores))
+            n_features_list.append(n_features_to_select)
+            
     # Find the optimal number of features based on cross-validation scores
     n_features_ = n_features_list[np.argmax(scores)]
     grid_scores_ = scores
     # Refit the model with the optimal number of features
-    estimator_ = PLSRegression(n_components=n_features_)#clone(estimator)
+    estimator_ = PLSRegression(n_components=n_comp)#clone(estimator)
     rfe = RFE(estimator=estimator_, n_features_to_select=n_features_, step=step)
     rfe.fit(X, Y)
     support_ = rfe.support_
     ranking_ = rfe.ranking_
     rfeindex = np.where(support_ == True)[0]
+    
     dset = pd.DataFrame()
+    
     dset['attr'] = np.arange(len(X[0,:]))
     dset['selection'] = rfe.support_
-    dset['importance'] = estimator.score(X, Y)
-    return dset, rfeindex
+
+    # dset['importance'] = estimator.score(X, Y)
+    return dset, rfeindex, grid_scores_
--- Code/WLD_DEPTH_MAPPING_REGRESSION.py
+++ Code/WLD_DEPTH_MAPPING_REGRESSION.py
@@ -7,19 +7,17 @@ Created on Tue Sep 19 13:11:44 2023
 import pandas as pd
 import numpy as np
 import os
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+
 import pickle
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.model_selection import cross_val_score
-
 from WLD_DEPTH_MAPPING_FUNCTIONS import RFECV_PLSR, RFECV_RF, regression_results
 
-path = 'C:\\Users\\syk32\\Dropbox\\\WLD_DEPTH_MAPPING'
+path = 'C:\\Users\\syk32\\Siyoon_Box Dropbox\Kwon Siyoon\\WLD_DEPTH_MAPPING' #add main path
 def change_dir(path):
     os.chdir(path)
     
-change_dir('C:\\Users\\syk32\\Dropbox\\\WLD_DEPTH_MAPPING')
+change_dir('C:\\Users\\syk32\\Siyoon_Box Dropbox\Kwon Siyoon\\WLD_DEPTH_MAPPING')
 print('dir:', os.getcwd())
 
 #%%Data load & model selection
@@ -27,13 +25,13 @@ print('dir:', os.getcwd())
 df_2021a = pd.read_csv(path+'\\Input\\H_Rrs_df2021a_window_3.csv', index_col=False)
 df_2021b = pd.read_csv(path+'\\Input\\H_Rrs_df2021b_window_3.csv', index_col=False)
 
-camp = 'total'#'2021a' or '2021b' or 'total'
+camp = '2021a'#'2021a' or '2021b' or 'total'
 
-if camp =='2021a':
+if camp =='2021b': #spring
     target_df = df_2021a
-elif camp =='2021b':
+elif camp =='2021b': #fall
     target_df = df_2021b
-elif camp =='total':
+elif camp =='total': #combined data
     target_df = pd.concat([df_2021a, df_2021b])
 
 target_df =target_df[target_df['river_dept']>0]
@@ -49,48 +47,134 @@ spectrum = spectrum[~nan_mask_x]
 
 #%%hyperparameter tuning for RF
 from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 best_max_depth = None
+best_n_estimators = None
 best_rmse = float('inf')
 X = spectrum.values
 Y = depth.values
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2,  random_state=20)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=20)
 
-for max_depth in range(1, 31):
-    estimator = RandomForestRegressor(
-        n_estimators=50,   
-        max_depth=max_depth,  
-        n_jobs=4,            
-        random_state=20      
-    )
-    
-    #Training
-    estimator.fit(X_train, y_train)
-    
-    # Prediction
-    y_train_pred = estimator.predict(X_train)
-    y_test_pred = estimator.predict(X_test)
-    
-    train_rmse = np.sqrt(mean_squared_error(y_train, y_train_pred))
-    test_rmse = np.sqrt(mean_squared_error(y_test, y_test_pred))
-    
-    if test_rmse < best_rmse:
-        best_max_depth = max_depth
-        best_rmse = test_rmse
-    
-    print(f'max_depth = {max_depth}, Train RMSE = {train_rmse:.4f}, Test RMSE = {test_rmse:.4f}')
 
-print(f'Best max_depth = {best_max_depth}, Best Test RMSE = {best_rmse:.4f}')
+max_depth_values = list(range(1, 31, 3)) + [None]
+n_estimators = 100
 
+for max_depth in max_depth_values:
+        estimator = RandomForestRegressor(
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            n_jobs=4,
+            random_state=20
+        )
+        
+        # Training
+        estimator.fit(X_train, y_train)
+        
+        # Prediction
+        y_train_pred = estimator.predict(X_train)
+        y_test_pred = estimator.predict(X_test)
+        
+        train_rmse = np.sqrt(mean_squared_error(y_train, y_train_pred))
+        test_rmse = np.sqrt(mean_squared_error(y_test, y_test_pred))
+        
+        if test_rmse < best_rmse:
+            best_max_depth = max_depth
+            best_n_estimators = n_estimators
+            best_rmse = test_rmse
+        
+        print(f'max_depth = {max_depth}, n_estimators = {n_estimators}, Train RMSE = {train_rmse:.4f}, Test RMSE = {test_rmse:.4f}')
+
+print(f'Best max_depth = {best_max_depth}, Best n_estimators = {best_n_estimators}, Best Test RMSE = {best_rmse:.4f}')
+
+
+
+
+#%%n_comp
+X = spectrum.values
+Y = depth.values
+cv = 5 #cross-validation fold
+step = 1 #step to test features
+n_comp_list = np.arange(8)+2
+# Lists to store results:
+dsets = []
+rfeindices = []
+grid_scores_s = []
+
+for ii in n_comp_list:
+   dset, rfeindex, grid_scores_ = RFECV_PLSR(X,Y, step, cv ,ii) #def RFECV_PLSR(X,Y, step, cv, n_comp):
+   dsets.append(dset)         # Save the dataset for this iteration
+   rfeindices.append(rfeindex) # Save the ranking indices for this iteration
+   grid_scores_s.append(grid_scores_)
+
+np.savez(path+'\\dsets_plsr_'+camp+'.npz',*dsets)
+np.savez(path+'\\rfeindices_plsr_'+camp+'.npz', *rfeindices)
+np.savez(path+'\\grid_scores_s_plsr_'+camp+'.npz', *grid_scores_s)
+
+#%%
+data = np.load(path+'\\dsets_plsr.npz', allow_pickle=True)
+dsets_plsr = [data[f'arr_{i}'] for i in range(8)]
+
+# data = np.load(path+'\\rfeindices_plsr.npz', allow_pickle=True)
+# rfeindices_plsr = [data[f'arr_{i}'] for i in range(8)]
+
+data = np.load(path+'\\grid_scores_s_plsr.npz', allow_pickle=True)
+grid_scores_s_plsr = [data[f'arr_{i}'] for i in range(8)]
+
+#%%
+# frame = np.zeros(len(grid_scores_s_plsr), 90, )
+grid_scores_s_plsr2 = grid_scores_s_plsr.copy()
+dummy = np.zeros_like(grid_scores_s_plsr[0])+np.nan
+
+for i in np.arange(len(grid_scores_s_plsr)):
+   if len(grid_scores_s_plsr[i]) < len(grid_scores_s_plsr[0]):
+      gap = len(grid_scores_s_plsr[0]) - len(grid_scores_s_plsr[i])
+      
+      
+      # dummy[gap:] = grid_scores_s_plsr[i]
+      grid_scores_s_plsr2[i] = np.concatenate([np.full(gap, np.nan), grid_scores_s_plsr[i]])
+      
+
+grid_scores_s_plsr3 = np.reshape(np.concatenate(grid_scores_s_plsr2), (len(grid_scores_s_plsr), 90))
+#%%Plot grid
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['font.family'] = 'Arial'
+matplotlib.rcParams['font.size'] = 14
+plt.figure(figsize=(8, 3)) 
+plt.pcolormesh(np.sqrt(-grid_scores_s_plsr3), cmap='jet_r', vmin=3.5, vmax=5.5)  # 'viridis' is a colormap, you can choose any other vmin=4.5, vmax=6
+plt.colorbar()  
+
+plt.yticks(np.arange(0.5, len(grid_scores_s_plsr3), 1), [f'{int(val)}' for val in np.linspace(2, 9, len(grid_scores_s_plsr3))])
+
+plt.show()
+
+grid_scores_s_plsr4 = np.sqrt(-grid_scores_s_plsr3)
+min_value = np.nanmin(grid_scores_s_plsr4)
+min_position = np.unravel_index(np.nanargmin(grid_scores_s_plsr4), grid_scores_s_plsr4.shape)  # 최대값의 위치 찾기, nan은 무시
+
+# 결과 출력
+print("Min value:", min_value)
+print("(raw, col):", min_position)
+
+#%%
+rfeindex = pd.read_csv(path+'\\Model\\rfeindex_plsr.csv')
+
+
+total = np.floor(rfeindex['Wave'][rfeindex['Total'] == 1])
+wave_2021a = np.floor(rfeindex['Wave'][rfeindex['Spring'] == 1])
+wave_2021b = np.floor(rfeindex['Wave'][rfeindex['Fall'] == 1])
 #%%Model selection
 model_opt = '2' #1: PLSR, 2: RF
 best_max_depth = 20
 
 if model_opt == '1':  
-    estimator = PLSRegression(n_components=91)#
+    estimator = PLSRegression(n_components=8)#
     model = 'PLSR'
 elif model_opt == '2':  
-    estimator = RandomForestRegressor(n_estimators = 50,max_depth=best_max_depth, n_jobs=4, random_state=20) # you can customize all hyperparameters.
+    estimator = RandomForestRegressor(n_estimators = 50, max_depth=best_max_depth, n_jobs=-1, random_state=20) # you can customize all hyperparameters.
     model = 'RF'
     
 #%%Feature Selction - RFE-CV
@@ -100,7 +184,7 @@ cv = 5 #cross-validation fold
 step = 1 #step to test features
 
 if model_opt == '1':  
-    dset, rfeindex = RFECV_PLSR(X,Y, step, cv, estimator)#dset contains information on feature importance and indicates whether each feature was selected.
+    dset, rfeindex = RFECV_PLSR(X,Y, step, cv, estimator, 8)
     model = 'PLSR'
 elif model_opt == '2':  
     dset, rfeindex = RFECV_RF(X,Y, step, cv, estimator)
